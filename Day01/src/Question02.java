ff8f72aaf3157a392637b86a3720842b4c42d0ea
kashyaptushar1 <127180405+kashyaptushar1@users.noreply.github.com>
2023-11-01 06:52:09 +0530

    Add files via upload
    Day 1 of DSA (Data Structures and Algorithms) Course

---
+++ Day01/src/Question02.java
@@ -0,0 +1,8 @@
+// print the ascii value of 'U' character
+public class Question02 {
+    public static void main(String[] args) {
+        char c = 'U';
+        int a = c;
+        System.out.println(a);
+    }
+}
