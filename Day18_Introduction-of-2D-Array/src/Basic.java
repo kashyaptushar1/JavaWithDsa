eb99e153c7a5b51dd03a3a812fa1ddca1ad9b79a
Tushar kashyap <127180405+kashyaptushar1@users.noreply.github.com>
2023-11-18 09:41:58 +0530

    Add files via upload

---
+++ Day18_Introduction-of-2D-Array/src/Basic.java
@@ -0,0 +1,13 @@
+public class Basic {
+    public static void main(String[] args) {
+        int arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        // This is how you declare and initialize 2-D Array
+        for (int i = 0; i < 3; i++) {// This is how you print 2-D Array
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
