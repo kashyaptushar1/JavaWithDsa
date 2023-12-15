de299d39ec52220a5da4cbb1799d7b9901acd54a
Tushar kashyap <127180405+kashyaptushar1@users.noreply.github.com>
2023-12-15 16:21:38 +0530

    Update Main.java

--- Day01_Variables and Data type/src/Main.java
+++ Day01_Variables and Data type/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!,i am Tushar");
+        System.out.println("Hello world!,i am Tushar Kashyap");
     }
 }
