8ef41c6c5f680052b97a99bc5b11254080fee5a7
Tushar Kashyap <155888866+i-am-tushar@users.noreply.github.com>
2024-03-02 16:29:25 +0530

    Update Main.java

--- Day22_Introduction-of-ArrayList/src/Main.java
+++ Day22_Introduction-of-ArrayList/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world, Tushar is Here.");
+        System.out.println("I know Yor love guddu bhai! If you dont accept my pull request i will tell your whole college friend");
     }
 }
